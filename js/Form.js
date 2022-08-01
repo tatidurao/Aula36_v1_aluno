1b496f919bb039c452304b430c72bae07945dddd
tatidurao <91564423+tatidurao@users.noreply.github.com>
2022-08-01 13:46:47 -0300

    Add files via upload

--- js/Form.js
+++ js/Form.js
@@ -15,17 +15,7 @@ class Form {
     button.position(250, 200);
 
     button.mousePressed(function(){
-      input.hide();
-      button.hide();
-
-      var name = input.value();
-      
-      playerCount+=1;
-      player.update(name)
-      player.updateCount(playerCount);
-      var greeting = createElement('h3');
-      greeting.html("Hello " + name )
-      greeting.position(130, 160)
+     
     });
 
   }
